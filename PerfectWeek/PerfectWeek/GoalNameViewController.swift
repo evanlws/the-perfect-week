35c9e3d76179fa1b5c566f21717e9a6dafa4a361
Evan Lewis <evanlws12@gmail.com>
2017-02-19 16:31:49 -0500

    Adds collection view cells

---
+++ PerfectWeek/PerfectWeek/GoalNameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GoalNameViewController.swift
+//  PerfectWeek
+//
+//  Created by Evan Lewis on 2/19/17.
+//  Copyright © 2017 evanlewis. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class GoalNameViewController: UIViewController {
+
+    @IBOutlet weak var goalNameTextField: UITextField!
+
+    @IBAction func didTap(withSender saveButton: UIButton) {
+        let realm = try! Realm()
+
+        guard let goalText = goalNameTextField.text else { return }
+        let goal = Goal()
+        goal.name = goalText
+
+        do {
+            try realm.write {
+                realm.add(goal)
+                print("Saved Goal")
+            }
+        } catch {
+            print("Can't save")
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension GoalNameViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+}
+
+class Goal: Object {
+    dynamic var name: String = ""
+}
--- PerfectWeek/PerfectWeek/GoalsViewController.swift
+++ PerfectWeek/PerfectWeek/GoalsViewController.swift
@@ -7,24 +7,51 @@
 //
 
 import UIKit
+import RealmSwift
 
 class GoalsViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    var goals: Results<Goal>?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let goalCollectionViewNib = UINib(nibName: String(describing: GoalCollectionViewCell.self), bundle: nil)
+        collectionView.register(goalCollectionViewNib, forCellWithReuseIdentifier: String(describing: GoalCollectionViewCell.self))
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let realm = try! Realm()
+        goals = realm.objects(Goal.self)
+    }
 
 }
 
 extension GoalsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return (goals?.count ?? 0) + 1
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: UICollectionViewCell.self), for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GoalCollectionViewCell.self), for: indexPath) as? GoalCollectionViewCell else { return UICollectionViewCell() }
+        if indexPath.row != goals?.count {
+            cell.goalLabel.text = goals?[indexPath.row].name
+        }
+
         return cell
     }
 }
 
 extension GoalsViewController: UICollectionViewDelegate {
-
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let addGoalStoryboard = UIStoryboard(name: "AddGoal", bundle: nil)
+        if let addGoalViewController = addGoalStoryboard.instantiateInitialViewController() {
+            present(addGoalViewController, animated: true) {
+                self.collectionView.reloadData()
+            }
+        }
+    }
 }
