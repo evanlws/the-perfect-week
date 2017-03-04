6118319477576e311d6380b76b466b75009b8b46
Evan Lewis <evanlws12@gmail.com>
2017-03-04 15:20:33 -0500

    Adds basic collectionView and Goal Cell

---
+++ PerfectWeek/PerfectWeek/GoalCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  GoalCollectionViewCell.swift
+//  PerfectWeek
+//
+//  Created by Evan Lewis on 3/4/17.
+//  Copyright © 2017 evanlewis. All rights reserved.
+//
+
+import UIKit
+
+class GoalCollectionViewCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .orange // TODO: Remove
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PerfectWeek/PerfectWeek/GoalViewController.swift
+++ PerfectWeek/PerfectWeek/GoalViewController.swift
@@ -18,8 +18,12 @@ class GoalsViewController: UIViewController {
     fileprivate func setupCollectionView() {
         let collectionViewFlowLayout = UICollectionViewFlowLayout()
         collectionViewFlowLayout.itemSize = CGSize(width: 200, height: 200)
+        collectionViewFlowLayout.scrollDirection = .vertical
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
         collectionView.backgroundColor = .green // TODO: Remove
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(GoalCollectionViewCell.self, forCellWithReuseIdentifier: String(describing: GoalCollectionViewCell.self))
         view.addSubview(collectionView)
 
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -28,4 +32,22 @@ class GoalsViewController: UIViewController {
         collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
+}
+
+extension GoalsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GoalCollectionViewCell.self), for: indexPath) as? GoalCollectionViewCell {
+            return cell
+        }
+
+        return GoalCollectionViewCell(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+    }
+}
+
+extension GoalsViewController: UICollectionViewDelegate {
+
 }
