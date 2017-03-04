20b0cd061d334b2c61a495e18b070c20a7233344
Evan Lewis <evanlws12@gmail.com>
2017-03-04 13:32:56 -0500

    Strips MVP

---
+++ PerfectWeek/PerfectWeek/Goal.swift
@@ -0,0 +1,14 @@
+//
+//  Goal.swift
+//  PerfectWeek
+//
+//  Created by Evan Lewis on 3/4/17.
+//  Copyright © 2017 evanlewis. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Goal: Object {
+    dynamic var name: String = ""
+}
