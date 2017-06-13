cbc1b558b2ce545cb4ccf37f0e04cf0becd6e46c
Evan Lewis <evanlws12@gmail.com>
2017-06-12 21:33:59 -0400

    Adds gray to color palette

--- PerfectWeek/PerfectWeek/ColorLibrary.swift
+++ PerfectWeek/PerfectWeek/ColorLibrary.swift
@@ -13,7 +13,8 @@ enum ColorLibrary {
 	enum UIPalette {
 		static let primary = UIColor(red: 0.43, green: 0.27, blue: 0.89, alpha: 1.0)
 		static let warning = UIColor(red: 0.82, green: 0.00, blue: 0.11, alpha: 1.0)
-		static let accent = UIColor(red:0.53, green:0.83, blue:0.81, alpha:1.0)
+		static let accent = UIColor(red: 0.53, green: 0.83, blue: 0.81, alpha: 1.0)
+		static let gray = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
 	}
 
 }
