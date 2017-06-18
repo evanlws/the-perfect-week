ffc6cda4f191f7f170e320091744a401a156172a
Evan Lewis <evanlws12@gmail.com>
2017-06-18 19:43:45 -0400

    Adds rounded corners to stepper

--- PerfectWeek/PerfectWeek/Stepper.swift
+++ PerfectWeek/PerfectWeek/Stepper.swift
@@ -24,55 +24,91 @@ final class Stepper: UIView {
 	}
 
 	private let numberLabel = UILabel()
+	private let plusButton = UIButton()
+	private let minusButton = UIButton()
+	private let verticalSpacer = UIView()
+	private let horizontalSpacer = UIView()
+	private let backgroundView = UIView()
+
 	weak var delegate: StepperDelegate?
 
 	init() {
 		super.init(frame: .zero)
-		backgroundColor = .purple
 		configureViews()
+		configureConstraints()
 	}
 
 	private func configureViews() {
 		numberLabel.text = String(counter)
 		numberLabel.font = UIFont.systemFont(ofSize: 28)
 		numberLabel.textColor = ColorLibrary.UIPalette.primary
-		numberLabel.backgroundColor = .white
-		numberLabel.layer.borderColor = UIColor.black.cgColor
-		numberLabel.layer.borderWidth = 1
 		numberLabel.textAlignment = .center
 
-		let plusButton = UIButton()
 		plusButton.setTitle("+", for: .normal)
 		plusButton.titleLabel?.font = UIFont.systemFont(ofSize: 28)
-		plusButton.backgroundColor = .white
-		plusButton.layer.borderColor = UIColor.black.cgColor
-		plusButton.layer.borderWidth = 1
 		plusButton.setTitleColor(ColorLibrary.UIPalette.primary, for: .normal)
 		plusButton.addTarget(self, action: #selector(increment), for: .touchUpInside)
 
-		let minusButton = UIButton()
 		minusButton.setTitle("-", for: .normal)
 		minusButton.titleLabel?.font = UIFont.systemFont(ofSize: 28)
-		minusButton.backgroundColor = .white
-		minusButton.layer.borderColor = UIColor.black.cgColor
-		minusButton.layer.borderWidth = 1
 		minusButton.setTitleColor(ColorLibrary.UIPalette.primary, for: .normal)
 		minusButton.addTarget(self, action: #selector(decrement), for: .touchUpInside)
 
-		let stepperStackView = UIStackView(arrangedSubviews: [plusButton, minusButton])
-		stepperStackView.axis = .vertical
-		stepperStackView.distribution = .fillEqually
+		verticalSpacer.backgroundColor = ColorLibrary.BlackAndWhite.black
+		horizontalSpacer.backgroundColor = ColorLibrary.BlackAndWhite.black
 
-		let containerStackView = UIStackView(arrangedSubviews: [numberLabel, stepperStackView])
-		containerStackView.axis = .horizontal
-		containerStackView.distribution = .fillEqually
-		addSubview(containerStackView)
+		backgroundView.backgroundColor = ColorLibrary.BlackAndWhite.white
+		backgroundView.layer.borderWidth = 1
+		backgroundView.layer.borderColor = ColorLibrary.BlackAndWhite.black.cgColor
+		backgroundView.layer.cornerRadius = Constraints.gridBlock
 
-		containerStackView.translatesAutoresizingMaskIntoConstraints = false
-		containerStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-		containerStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-		containerStackView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-		containerStackView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+		addSubview(backgroundView)
+		addSubview(numberLabel)
+		addSubview(plusButton)
+		addSubview(minusButton)
+		addSubview(horizontalSpacer)
+		addSubview(verticalSpacer)
+	}
+
+	private func configureConstraints() {
+		backgroundView.translatesAutoresizingMaskIntoConstraints = false
+		numberLabel.translatesAutoresizingMaskIntoConstraints = false
+		plusButton.translatesAutoresizingMaskIntoConstraints = false
+		minusButton.translatesAutoresizingMaskIntoConstraints = false
+		horizontalSpacer.translatesAutoresizingMaskIntoConstraints = false
+		verticalSpacer.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			backgroundView.topAnchor.constraint(equalTo: topAnchor),
+			backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
+			backgroundView.leftAnchor.constraint(equalTo: leftAnchor),
+			backgroundView.rightAnchor.constraint(equalTo: rightAnchor),
+
+			verticalSpacer.topAnchor.constraint(equalTo: topAnchor),
+			verticalSpacer.bottomAnchor.constraint(equalTo: bottomAnchor),
+			verticalSpacer.widthAnchor.constraint(equalToConstant: 1.0),
+			verticalSpacer.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+			horizontalSpacer.leftAnchor.constraint(equalTo: verticalSpacer.rightAnchor),
+			horizontalSpacer.rightAnchor.constraint(equalTo: rightAnchor),
+			horizontalSpacer.heightAnchor.constraint(equalToConstant: 1.0),
+			horizontalSpacer.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+			numberLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 2.0),
+			numberLabel.rightAnchor.constraint(equalTo: verticalSpacer.leftAnchor, constant: 2.0),
+			numberLabel.topAnchor.constraint(equalTo: topAnchor, constant: 2.0),
+			numberLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2.0),
+
+			plusButton.leftAnchor.constraint(equalTo: verticalSpacer.rightAnchor, constant: 2.0),
+			plusButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -2.0),
+			plusButton.topAnchor.constraint(equalTo: topAnchor, constant: 2.0),
+			plusButton.bottomAnchor.constraint(equalTo: horizontalSpacer.topAnchor, constant: -2.0),
+
+			minusButton.leftAnchor.constraint(equalTo: verticalSpacer.rightAnchor, constant: 2.0),
+			minusButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -2.0),
+			minusButton.topAnchor.constraint(equalTo: horizontalSpacer.bottomAnchor, constant: 2.0),
+			minusButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2.0)
+		])
 	}
 
 	func increment() {
